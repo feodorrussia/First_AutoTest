df7c33ea7a91d2efc85c8b1835e9e2c6058ba4ac
feodorrussia <fvdelows@mail.ru>
2022-03-29 19:13:04 +0300

    Сделал всторой тест для корректных данных(первый сделал для не корректных данных)

--- AutoTest/src/test/java/org/test/ExampleTest.java
+++ AutoTest/src/test/java/org/test/ExampleTest.java
@@ -1,18 +1,31 @@
 package org.test;
 
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExampleTest {
 
     @Test
-    public void LoginTest() {
-        User user = new User("Bla Blabla", "bla1996", "bla_password");
-        User user2 = new User("Фёдор Белоус", "blabla2014", "blabla_password");
+    public void LoginTestEQ() {
+        User user1 = new User("Alan Rick", "bla1996@gmail.ru", "bla_password1234");
+        User user = new User("Bla Blabla", "+79631074312", "m123467890");
+        open("https://ok.ru");
+        LoginPage lp = new LoginPage();
+        MainPage mp = lp.login(user);
+        assertEquals(mp.getName(), user.getName());
+        closeWebDriver();
+    }
+
+    @Test
+    public void LoginTestNotEQ(){
+        User user = new User("Alan Rick", "bla1996@gmail.ru", "bla_password1234");
         open("https://ok.ru");
         LoginPage lp = new LoginPage();
         MainPage mp = lp.login(user);
         assertNotEquals(mp.getName(), user.getName());
+        closeWebDriver();
     }
 }
